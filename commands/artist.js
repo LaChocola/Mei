80ec459d75ad441f7f917c529d33ee2667e771ab
LaChocola <thenekoluv@gmail.com>
2017-09-25 00:11:07 -0500

    fixed the issue where people would not be able to add links if they had custom names

--- commands/artist.js
+++ commands/artist.js
@@ -17,10 +17,16 @@ module.exports = {
     var id = mentioned.id
     var hands = [ ":ok_hand::skin-tone-1:", ":ok_hand::skin-tone-2:", ":ok_hand::skin-tone-3:", ":ok_hand::skin-tone-4:", ":ok_hand::skin-tone-5:", ":ok_hand:"]
     var hand = hands[Math.floor(Math.random() * hands.length)]
-    if (!(data.people[id])) {
-      data.people[id]= {};
-      data.people[id].links = {}
+    if (!(data.people[id].links)) {
+			if (data.people[id]) {
+				data.people[id].links = {}
+				return;
+			} else {
+	      data.people[id]= {};
+				data.people[id].links = {}
+			}
     }
+
     if (args.includes("add")) {
       if (mentioned.id != m.author.id) {
         Bot.createMessage(m.channel.id, "Okay....but that isnt you");
