defb29456e0af6acfdb71e70fd3fd64a704f7b97
LaChocola <thenekoluv@gmail.com>
2017-10-10 07:29:11 -0500

    New Features

---
+++ commands/art.js
@@ -0,0 +1,56 @@
+module.exports = {
+    main: function(Bot, m, args) {
+          Bot.getMessages("356133586686181377", parseInt(5000)).then(function(msgs) {
+            var art = {}
+              for (var msg of msgs) {
+                if (msg.content.includes("pastebin.com")) {
+                  art[msg.content] = msg.author.id
+                }
+                if (msg.attachments[0]) {
+                  art[msg.attachments[0].url] = msg.author.id
+                }
+              }
+              Bot.sendChannelTyping(m.channel.id).then(async () => {
+                var number = Math.floor(Math.random() * Object.entries(art).length);
+                var list = Object.entries(art)
+                var chosen = list[number]
+                var author = m.channel.guild.members.get(chosen[1]).nick || m.channel.guild.members.get(chosen[1]).username
+                var url = m.channel.guild.members.get(chosen[1]).avatarURL
+                if (chosen[0].includes("pastebin.com")) {
+                  const data = {
+                    "content": "A Random piece by ***"+author+"*** from <#356133586686181377>~",
+                    "embed": {
+                      "color": 0xA260F6,
+                      "title": chosen[0],
+                      "url": chosen[0],
+                      "author": {
+                        "name": author,
+                        "icon_url": url
+                      }
+                    }
+                  };
+                  Bot.createMessage(m.channel.id, data);
+                  return;
+                }
+                else {
+                  const data = {
+                    "content": "A Random piece by ***"+author+"*** from <#356133586686181377>~",
+                    "embed": {
+                      "color": 0xA260F6,
+                      "image": {
+                        "url": chosen[0]
+                      },
+                      "author": {
+                        "name": author,
+                        "icon_url": url
+                      }
+                    }
+                  };
+                  Bot.createMessage(m.channel.id, data);
+                }
+              });
+          });
+          return;
+  },
+  help: "Clean stuff" // add description
+}
---
+++ commands/fetish.js
@@ -0,0 +1,214 @@
+const _ = require("../people.js");
+var data = _.load();
+module.exports = {
+	main: function(Bot, m, args) {
+    	var name1 = m.cleanContent.replace("!fetish ", "")
+			function capFirstLetter(string) {
+				return string.charAt(0).toUpperCase() + string.slice(1);
+			}
+      var isThisUsernameThatUsername = function(member) {
+        var memberName = member.nick || member.username
+        if (memberName.toLowerCase() == name1.toLowerCase()) {
+          return true;
+          }
+        }
+    var member = m.guild.members.find(isThisUsernameThatUsername)
+    var mentioned = m.mentions[0] || member || m.author
+    var name = m.channel.guild.members.get(mentioned.id).nick || mentioned.username
+    var likes = []
+		var dislikes = []
+    var commonLikes = []
+		var commonDislikes = []
+    var id = mentioned.id
+    var hands = [ ":ok_hand::skin-tone-1:", ":ok_hand::skin-tone-2:", ":ok_hand::skin-tone-3:", ":ok_hand::skin-tone-4:", ":ok_hand::skin-tone-5:", ":ok_hand:"]
+    var hand = hands[Math.floor(Math.random() * hands.length)]
+		if (!(data.people[id])) {
+			data.people[id]= {};
+		}
+    if (!(data.people[id].fetishes)) {
+			data.people[id].fetishes = {}
+    }
+    if (args.includes("search")) {
+      var incomingEntries = name1.replace("search ", "").replace(": ", " ").split(" ")
+      var incoming = [];
+      var iterator = incomingEntries.entries()
+      for (let e of iterator) {
+        incoming.push(capFirstLetter(e[1]))
+      }
+      var matches = Object.keys(data.people).filter(k => data.people[k].fetishes && data.people[k].fetishes[`${incoming[0]}`] == "like")
+      if (matches.length < 1) {
+        Bot.createMessage(m.channel.id, "No matches found");
+        return;
+      } else {
+        var usernames = []
+        for (const match of matches) {
+					if (m.channel.guild.members.get(match)) {
+	          var properName = m.channel.guild.members.get(match).nick || m.channel.guild.members.get(match).username
+	          usernames.push("***"+properName+"***")
+					}
+        }
+        Bot.createMessage(m.channel.id, "Users that like `" + incoming[0]+"`:\n\n"+usernames.join("\n"));
+        return;
+      }
+    }
+    if (args.includes("add")) {
+      if (mentioned.id != m.author.id) {
+        Bot.createMessage(m.channel.id, "Okay....but that isnt you");
+        return;
+      }
+      var incomingEntries = name1.replace("add ", "").replace(": ", " ").split(" ")
+			var incoming = [];
+			var iterator = incomingEntries.entries()
+			for (let e of iterator) {
+				incoming.push(capFirstLetter(e[1]))
+			}
+      if (data.people[id].fetishes[incoming[0]]) {
+        Bot.createMessage(m.channel.id, "That's already been added, silly~");
+        return;
+      }
+      else {
+				if (incoming.indexOf("Dislike") > -1) {
+          incoming.splice(incoming.indexOf("Dislike"), 1)
+					data.people[id].fetishes[incoming[0]] = "dislike"
+					_.save(data)
+					Bot.createMessage(m.channel.id, "Added Dislike: **" + incoming[0] + "** " + hand);
+					return;
+				}
+				else {
+					data.people[id].fetishes[incoming[0]] = "like"
+					_.save(data)
+					Bot.createMessage(m.channel.id, "Added **" + incoming[0] + "** " + hand);
+					return;
+				}
+      }
+    }
+    if (args.toLowerCase().includes("remove")) {
+      if (mentioned.id != m.author.id) {
+        Bot.createMessage(m.channel.id, "Okay....but that isnt you");
+        return;
+      }
+      var incoming = name1.replace("remove ", "").replace(": ", " ").split(" ")
+      if (data.people[id].fetishes[capFirstLetter(incoming[0])]) {
+        delete data.people[id].fetishes[capFirstLetter(incoming[0])]
+        _.save(data)
+        Bot.createMessage(m.channel.id, "Removed: **" + incoming[0] + "** from your fetish list" + hand);
+        return;
+      }
+      else {
+        Bot.createMessage(m.channel.id, "Sorry, I couldnt find **" + incoming[0] + "** in your fetish list");
+        return;
+      }
+    }
+    if (Object.keys(data.people[id].fetishes).length < 1) {
+      Bot.createMessage(m.channel.id, "I could find any fetish list for **" + name + "** :(");
+      return;
+    }
+    else {
+			var fetishes = data.people[id].fetishes
+      if (mentioned.id != m.author.id) {
+        for (const [key, value] of Object.entries(fetishes)) {
+          if (data.people[m.author.id].fetishes[key]) {
+          if (data.people[m.author.id].fetishes[key] == "dislike" && data.people[id].fetishes[key] == "dislike") {
+            commonDislikes.push(`${key}`)
+            continue;
+          }
+          else if (data.people[m.author.id].fetishes[key] == "like" && data.people[id].fetishes[key] == "like") {
+            commonLikes.push(`${key}`)
+            continue;
+          }
+        }
+      }
+    }
+
+      for (const [key, value] of Object.entries(fetishes)) {
+        if (value == "like") {
+          likes.push(`${key}`)
+        }
+        if (value == "dislike") {
+          dislikes.push(`${key}`)
+        }
+      }
+			if (likes.length < 1) {
+				likes.push("None")
+			}
+			if (dislikes.length < 1) {
+				dislikes.push("None")
+			}
+      if (mentioned.id == m.author.id) {
+          Bot.createMessage(m.channel.id, { content: "",
+            embed: {
+                color: 0xA260F6,
+                title:  Object.keys(data.people[id].fetishes).length + " fetishes for **" + name + "**",
+                fields: [
+                {
+                    name: ':green_heart: Likes: '+likes.length,
+                    value: likes.join("\n"),
+                    inline: true
+                },
+                {
+                    name: ':x: Dislikes: '+dislikes.length,
+                    value: dislikes.join("\n"),
+                    inline: true
+                }],
+                author: {
+                  name: name,
+                  icon_url: mentioned.avatarURL
+                }
+            }
+        });
+        return;
+      }
+      else {
+        if (commonLikes.length < 1) {
+          commonLikes.push("None")
+        }
+        if (commonDislikes.length < 1) {
+          commonDislikes.push("None")
+        }
+          Bot.createMessage(m.channel.id, { content: "",
+            embed: {
+                color: 0xA260F6,
+                fields: [
+                {
+                    name: ':green_heart: Likes: '+likes.length,
+                    value: likes.join("\n")+" \n \u200b",
+                    inline: true
+                },
+                {
+                    name: "\u200b",
+                    value: "\u200b",
+                    inline: true
+                },
+                {
+                    name: ':x: Dislikes: '+dislikes.length,
+                    value: dislikes.join("\n")+" \n \u200b",
+                    inline: true
+                },
+                {
+                    name: ':green_heart: Common Likes: '+commonLikes.length,
+                    value: commonLikes.join("\n"),
+                    inline: true
+                },
+                {
+                    name: "\u200b",
+                    value: "\u200b",
+                    inline: true
+                },
+                {
+                    name: ':x: Common Dislikes: '+commonDislikes.length,
+                    value: commonDislikes.join("\n"),
+                    inline: true
+                }],
+                author: {
+                  name: name,
+                  icon_url: mentioned.avatarURL
+                }
+            }
+        });
+        return;
+      }
+    }
+
+	},
+	help: "Add custom fetishes"
+}
