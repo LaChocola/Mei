31ac8f2b233329f770f91f9e081b1a5d9c32cf82
LaChocola <thenekoluv@gmail.com>
2017-10-12 04:44:18 -0500

    making sure to return when there isnt an input

--- commands/say.js
+++ commands/say.js
@@ -3,6 +3,7 @@ module.exports = {
 		var msg = m.cleanContent.replace("!say ", "");
 		if (m.content == "!say") {
 			Bot.createMessage(m.channel.id, "Please add something to say. i.e. ``!say <whatever>``")
+			return
 		}
 		else {
 		Bot.createMessage(m.channel.id, msg);
