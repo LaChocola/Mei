1c317245c7442595480c8fb00f2db2ac015d330e
LaChocola <thenekoluv@gmail.com>
2017-10-27 06:12:28 -0500

    Rest in peace, Watson Keys

--- commands/markov.js
+++ commands/markov.js
@@ -70,30 +70,6 @@ module.exports = {
                     }
             }
         });
-        var msg = messages
-        tone_analyzer.tone({ text: msg },
-      function(err, tone) {
-        if (err) {
-          console.log(err)
-          Bot.createMessage(m.channel.id, err);
-        }
-        else {
-          let tones = tone.document_tone.tone_categories; // All tone categories
-          let topTones = {}; // Store all tones in here
-
-        tones.forEach(t => { // Iterate through categire
-        let [first] = t.tones.sort((a, b) => b.score - a.score); // Sort by descending score and get top one.
-        first = {name: first.tone_name, percent: (first.score * 100).toFixed(1)}; // Get the tone's name
-        topTones[t.category_id.split('_')[0]] = first; // Assign the name of the tone category to the top tone and add it to the tone object.
-    });
-        var tone1 = "Average Emotional Tone: ***" + topTones.emotion.name + "***  | Score " + topTones.emotion.percent + "%"
-        var tone2 = "Average Language Tone: ***" + topTones.language.name + "***  | Score " + topTones.language.percent + "%"
-        var tone3 = "Average Social Tone: ***" + topTones.social.name + "***  | Score " + topTones.social.percent + "%"
-        Bot.createMessage(m.channel.id, "Users Tones: " + "\n" +  tone1 + "\n" + tone2 + "\n"+ tone3);
-
-        // Bot.createMessage(m.channel.id, "Tones Detected: ***" + tone1 + "*** and ***" + tone2 + "***");
-        }
-    });
     });
     },
     help: "Markov and Watson"
