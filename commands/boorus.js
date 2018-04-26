9ea4b5d9aa04675ab9745b1c1c370aab0a5579dc
LaChocola <thenekoluv@gmail.com>
2018-04-26 16:34:21 -0500

    nice typo

--- commands/boorus.js
+++ commands/boorus.js
@@ -1,6 +1,6 @@
 module.exports = {
     main: function(Bot, m, args, prefix) {
-        Bot.createMessage(m.channel.id, "**Total Boorus Availible:**\n \n**Default Booru:** *giantessbooru.com* \n**Other Availbe Boours/Aliases:** giantessbooru, gtsbooru, gbooru, giantessbooru.com, e6, e621, e9, e926, hh, hypo, hypohub, db, dan, danbooru, kc, konac, kcom, kn, konan, knet, yd, yand, yandere, gb, gel, gelbooru, r34, rule34, sb, safe, safebooru, tb, tbib, big, xb, xbooru, yh, you, youhate, do, doll, dollbooru, pa, paheal, lb, lol, loli, lolibooru, dp, derp, derpi, derpibooru")
+        Bot.createMessage(m.channel.id, "**Total Boorus Availible:**\n \n**Default Booru:** *giantessbooru.com* \n**Other Availbe Boorus/Aliases:** giantessbooru, gtsbooru, gbooru, giantessbooru.com, e6, e621, e9, e926, hh, hypo, hypohub, db, dan, danbooru, kc, konac, kcom, kn, konan, knet, yd, yand, yandere, gb, gel, gelbooru, r34, rule34, sb, safe, safebooru, tb, tbib, big, xb, xbooru, yh, you, youhate, do, doll, dollbooru, pa, paheal, lb, lol, loli, lolibooru, dp, derp, derpi, derpibooru")
     },
     help: "Boorus availible in ``!booru``"
 }
