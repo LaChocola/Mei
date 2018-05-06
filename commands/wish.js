828e579c45bf012f6267cc9fcf7b9a87733c4137
LaChocola <thenekoluv@gmail.com>
2018-05-06 14:08:30 -0500

    Included my shitty alphabets if someone wants to copy !wish

--- commands/wish.js
+++ commands/wish.js
@@ -1,4 +1,6 @@
 const Jimp = require('jimp');
+// This version of Jimp has an alphabet I created to emulated the DeviantArt username font "Trebuchet" and timestamp font
+// They are both availible in a zip on http://www.filedropper.com/deviantfonts and need to be added to jimp via node_modules>jimp>fonts
 var time = new Date().toDateString().slice(4).replace(` ${new Date().getFullYear()}`, `, ${new Date().getFullYear()}`)
 module.exports = {
     main: function(Bot, m, args, prefix) {
