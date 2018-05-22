b92443cb9d1a247109b798a5322e06df876d518b
LaChocola <thenekoluv@gmail.com>
2018-05-22 02:04:11 -0500

    Cleaned up text

--- commands/uptime.js
+++ commands/uptime.js
@@ -4,7 +4,7 @@ module.exports = {
         var prettyUptime = prettyMs(Bot.uptime, {
             verbose: true
         })
-        Bot.createMessage(m.channel.id, ":alarm_clock: " + prettyUptime + " :alarm_clock:")
+        Bot.createMessage(m.channel.id, "I have been running for:\n:alarm_clock: " + prettyUptime + " :alarm_clock:")
     },
     help: "Shows bot uptime"
 }
