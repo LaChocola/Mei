c90e74a555625c3d0c0f2004ae3f866ace300372
LaChocola <thenekoluv@gmail.com>
2018-07-02 10:30:47 -0500

    added failsafe for json database corruption

--- people.js
+++ people.js
@@ -2,7 +2,24 @@ const fs = require("fs");
 
 module.exports = {
     load: function() {
-        return JSON.parse(fs.readFileSync("./db/people.json"));
+        var data = fs.readFileSync("./db/people.json", "utf8")
+        if (data.includes("161027274764713984")) {
+            try {
+                return JSON.parse(fs.readFileSync("./db/people.json"));
+            } catch (err) {
+                console.log(err)
+            }
+        } else {
+            console.log("JSON error, attempting restore");
+            try {
+                var backup = JSON.parse(fs.readFileSync("./dbackup/people.json"));
+                fs.writeFileSync("./db/people.json", JSON.stringify(backup, null, "\t"))
+                console.log("Restore Successful");
+                return JSON.parse(fs.readFileSync("./db/people.json"));
+            } catch (err) {
+                console.log(err)
+            }
+        }
     },
     save: function(data) {
         fs.writeFileSync("./db/people.json", JSON.stringify(data, null, "\t"));
