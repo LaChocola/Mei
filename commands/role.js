37ae8ef5fd8345669f76ffaf53658b40dad1e234
LaChocola <thenekoluv@gmail.com>
2018-07-23 02:47:12 -0500

    transitioned to new role storage structure

--- commands/role.js
+++ commands/role.js
@@ -1,3 +1,5 @@
+const _ = require("../servers.js");
+var data = _.load();
 module.exports = {
     main: function(Bot, m, args, prefix) {
         var hands = [":ok_hand::skin-tone-1:", ":ok_hand::skin-tone-2:", ":ok_hand::skin-tone-3:", ":ok_hand::skin-tone-4:", ":ok_hand::skin-tone-5:", ":ok_hand:"]
@@ -18,222 +20,37 @@ module.exports = {
             Bot.createMessage(m.channel.id, "***One*** space Please");
             return;
         }
-        var roles = {
-            "big": "356005613630914560",
-            "streamengine": "358727281377542146",
-            "switch": "356005806681882656",
-            "creator": "358537297538318336",
-            "small": "356005120816971786",
-            "neutral": "358535595892670465",
-            "degenerate": "372625251819061249"
-        }
-        if (m.channel.guild.id == "326172270370488320") {
-          var roles = {
-            "giantess": "326479753806872586",
-            "writers": "419777829979553793",
-            "gentle": "424457908617609216",
-            "artist": "326513572966432769",
-            "switch": "326492362123640843",
-            "expansion": "343903114484776961",
-            "micro": "371363527388299283",
-            "tiny": "326479817165766657",
-            "nano": "388340430124613642",
-            "giga": "422618197917696011",
-            "giant": "326479986527567872",
-            "furry": "326479916553994260",
-            "monster boy/girl": "433835189425143808",
-            "normies": "424414455951261716",
-            "ask to dm": "428352641282736149",
-            "cruel": "424457948220096513",
-            "no dms": "419200356183179264",
-            "no rps": "420375117990854656"
+        var roles = undefined
+        if (data[guild.id]) {
+          if (data[guild.id].roles) {
+            if (Object.keys(data[guild.id].roles)[0]) {
+              var roles = data[guild.id].roles
+            }
           }
         }
-        if (m.channel.guild.id == "435239066225868800") {
-          var roles = {
-            "switch": "436776743936983055",
-            "macro": "436776841701752832",
-            "micro": "436776912539615232"
-          }
-        }
-        if (m.channel.guild.id == "416487280237215744") {
-          var roles = {
-            "worshipper": "416496472926715917",
-            "toy": "416852773196267521",
-            "commoner": "416852902313721866",
-            "switch": "418127170070642690",
-            "rpfriendly": "420378132810301441",
-            "rpunfriendly": "420378244316004362",
-            "giantess": "416852995385327637",
-            "goddess": "416853083633745939",
-            "creator": "418657403672854529"
-          }
-        }
-        if (m.channel.guild.id == "372587460800282625") {
-          var roles = {
-            "tiny": "372604586978181120",
-            "giantess/giant": "372605057642004481",
-            "normal sized": "376111382103326730",
-            "giga": "376157347594371072",
-            "mega": "376157449901703180",
-            "macro": "376157502720573441",
-            "micro": "376157654525149194",
-            "tamaño normal": "377173536474660864",
-            "gigante/giganta": "379310900210958340"
-          }
-        }
-        if (m.channel.guild.id == "433471999184994304") {
-          var roles = {
-            "straight": "434428924144123904",
-            "breasts": "437253713598480394",
-            "death": "434429395852328960",
-            "musk": "434429520682942465",
-            "normal": "434381553066573824",
-            "full tour vore": "434429672441511956",
-            "no lewd": "435179926576955402",
-            "taken": "435137851815100426",
-            "dm unfriendly": "434396180298989569",
-            "mega macro": "434383340976472065",
-            "big": "434381635547299854",
-            "predator": "434428839238828032",
-            "single": "435137937051484172",
-            "big*": "433481603256156160",
-            "looking for owner": "434429999794225163",
-            "soft vore": "434429394073681943",
-            "digestion": "434429670545555467",
-            "rough": "434429262989099008",
-            "(dnd) team wow": "437343826802245652",
-            "bi": "434428924605366275",
-            "micro": "434380830295457793",
-            "male": "434377518984724480",
-            "gas": "434429520641130497",
-            "gentle": "434429262616068117",
-            "small": "433481481000714240",
-            "extremely normal": "437345400211505163",
-            "hard vore": "434429395545882634",
-            "watersports": "434429259751096352",
-            "insertion": "435516083693289482",
-            "feet": "434429261072564234",
-            "scat": "434429087654608908",
-            "giga macro": "434383518009655296",
-            "teasing": "437253611207262208",
-            "smaller": "433481334200074240",
-            "writer": "433685793337376788",
-            "stink": "434429521396105217",
-            "artist": "433685836823920640",
-            "butts": "437253654311862272",
-            "no roleplay": "434429809872076810",
-            "cruel": "434453924406231040",
-            "non-switch": "434428693058813962",
-            "looking for pet": "434429999643361281",
-            "tiny": "434380949489451008",
-            "female": "434377486873133056",
-            "no death": "434429397018214401",
-            "futa": "434428922852147202",
-            "prefers to be smaller": "434429808559259649",
-            "tera macro": "434882461046931456",
-            "normal*": "433481577096544276",
-            "in dnd": "434821543504248853",
-            "little": "434381123293020161",
-            "furry": "434429261613629471",
-            "gay": "434429086174150676",
-            "smallest": "433479403473731595",
-            "prefers to be bigger": "434429808135634944",
-            "trans": "437346533956911125",
-            "macro": "434381791026085890",
-            "scalie": "434429522037833743",
-            "avian": "434429522818105350",
-            "asexual": "437253757739466752",
-            "prey": "434428897405435904",
-            "dm friendly": "434395988656914443",
-            "gore": "434429397114683405",
-            "switch": "434396480271548446",
-            "streamer": "434429086232739841",
-            "mouthplay": "434429670578978826",
-            "crush": "434429087411470337",
-            "speck": "434380266191192064",
-            "bigger": "433481625679036418"
-          }
-        }
-        if (m.channel.guild.id == "435239066225868800") {
-          var roles = {
-            "prefer to be smaller": "437016553708584970",
-            "gay": "436953412886724666",
-            "gas": "436955619673505802",
-            "feet": "436954888988262424",
-            "mouthplay": "437017214332436480",
-            "death": "436955559393099777",
-            "straight": "436953359199764491",
-            "big": "436940727126327297",
-            "scat": "436950734244675595",
-            "micro": "436776912539615232",
-            "digestion": "437017022484840448",
-            "prefer to be bigger": "437016482254422027",
-            "normal": "436940079596961792",
-            "bi": "436953467437973515",
-            "dm friendly": "436955757896794133",
-            "doesn't roleplay": "437017100423528469",
-            "furry": "436955014339100676",
-            "no death": "437018012026011648",
-            "lewd": "450314741106081792",
-            "full tour vore": "437017535326584851",
-            "crush": "436953533250797580",
-            "soft vore": "436955241259335700",
-            "male": "436953118136074292",
-            "gentle": "436955117833420800",
-            "speck": "436945234480463882",
-            "futa": "436953298491277312",
-            "macro": "436776841701752832",
-            "tiny": "436941075232456705",
-            "rough": "436955180437602306",
-            "switch": "436776743936983055",
-            "musk": "436955663407775745",
-            "taken": "437016181480751114",
-            "female": "436953231277555742",
-            "watersports": "436950659280142336",
-            "hard vore": "436955306602397698",
-            "butts": "451306588645490688",
-            "asexual": "437016404634370049",
-            "insertion": "437016324611375124",
-            "single": "437016142708604950",
-            "giga": "436939944817197057",
-            "dm unfriendly": "436955828508164116"
-          }
-        }
-        if (m.channel.guild.id == "396122792531197952") {
-          var roles = {
-            "pirate party": "397360783216082946",
-            "head of the giant party": "397360293052940298",
-            "goddess": "397651429239816203",
-            "human male": "397661187321626625",
-            "tiny female": "397665523372130304",
-            "liberal": "397985828653236234",
-            "gods": "397653419718082561",
-            "republicans": "396156426239606785",
-            "liberal democrats (uk)": "398213298732138498",
-            "giant party": "397196918926737409",
-            "tiny party": "397119025223827456",
-            "green party": "397184860252405771",
-            "libertarian": "396157092047749131",
-            "human female": "397661693238575115",
-            "tiny male": "397663371404640256",
-            "socialist": "397122591849971712",
-            "micro": "397669886014259212",
-            "head of the giantess party": "397360289307426816",
-            "giant": "397659084037423115",
-            "giantess": "397656534873800704",
-            "dragon": "398212293470453773",
-            "switch": "397968217890357248",
-            "dragon nationalist": "398212875795300352",
-            "democrats": "396155836248096768",
-            "head of the tiny party": "397360295242235904",
-            "giantess party": "397119269604950017"
-          }
+        if (!roles) {
+          Bot.createMessage(m.channel.id, "No roles have been set up yet. Use `!edit roles` to add and remove assignable roles. (Requires Moderator Permissions)");
+          return;
         }
 
         if (m.mentions.length > 0 && m.mentions[0].id != m.author.id) {
             Bot.createMessage(m.channel.id, "You can only assign roles to yourself");
             return;
+        }
+        if (m.content.includes("list")) {
+          if (!data[guild.id].roles) {
+            Bot.createMessage(m.channel.id, "There are no roles set up in this server, to add roles, please use `!edit roles add <rolename>`");
+            return;
+          }
+          var roles = Object.keys(data[guild.id].roles)
+          Bot.createMessage(m.channel.id, {
+              content: "",
+              embed: {
+                  color: 0xA260F6,
+                  title: roles.length + " roles are available:",
+                  description: " \n" + roles.join("\n")
+              }
+          });
         }
         if (m.content.includes("add")) {
             if (!m.content.includes(" | ")) {
@@ -244,19 +61,19 @@ module.exports = {
                         return Bot.createMessage(m.channel.id, hand + " Successful added: " + content).then((msg) => {
                             return setTimeout(function() {
                                 Bot.deleteMessage(msg.channel.id, msg.id, "Timeout")
-                            }, 5000) && setTimeout(function() {
+                            }, 7000) && setTimeout(function() {
                                 Bot.deleteMessage(m.channel.id, m.id, "Timeout")
-                            }, 5000)
+                            }, 7000)
                         })
                     })
                     return;
                 } else {
-                    Bot.createMessage(m.channel.id, content + ": Not found").then((msg) => {
+                    Bot.createMessage(m.channel.id, "'" + content + "' is not a role that as been set up in this server").then((msg) => {
                         return setTimeout(function() {
                             Bot.deleteMessage(msg.channel.id, msg.id, "Timeout")
-                        }, 5000) && setTimeout(function() {
+                        }, 7000) && setTimeout(function() {
                             Bot.deleteMessage(m.channel.id, m.id, "Timeout")
-                        }, 5000)
+                        }, 7000)
                     })
                     return;
                 }
