4638053fef017bd07937784b4a5f65bdda9620b7
LaChocola <thenekoluv@gmail.com>
2018-07-23 02:48:44 -0500

    improved display for roles

--- commands/allroles.js
+++ commands/allroles.js
@@ -4,12 +4,12 @@ module.exports = {
         var roleSearch = function(role) {
             var roleName = role.name
             if (roleName != "undefined") {
-                console.log(`"${roleName}": "${role.id}",`);
+                console.log(`"${roleName.toLowerCase()}": "${role.id}",`);
                 return roleName;
             }
         }
         var roles = m.guild.roles.map(roleSearch)
-        Bot.createMessage(m.channel.id, "**" + roles.join("** | **") + "**");
+        Bot.createMessage(m.channel.id, roles.join("  |  "));
     },
     help: "Get your role info"
 }
