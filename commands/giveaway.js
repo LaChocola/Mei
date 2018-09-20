2a572a503b1b3c332c8f73dc51ccf602d1cb1131
LaChocola <thenekoluv@gmail.com>
2018-09-19 20:56:33 -0500

    giveaway command is fully functional now

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -1,5 +1,5 @@
-const _ = require("../data.js");
-const data = _.load();
+const _ = require("../servers.js");
+var data = _.load();
 
 function promiseTimeout(time) {
     return new Promise(resolve => setTimeout(() => resolve(), time));
@@ -7,6 +7,9 @@ function promiseTimeout(time) {
 module.exports = {
     main: function(Bot, m, args, prefix) {
         return new Promise((resolve, reject) => {
+            var guild = m.channel.guild
+            var hands = [":ok_hand::skin-tone-1:", ":ok_hand::skin-tone-2:", ":ok_hand::skin-tone-3:", ":ok_hand::skin-tone-4:", ":ok_hand::skin-tone-5:", ":ok_hand:"]
+            var hand = hands[Math.floor(Math.random() * hands.length)]
             var time = 0.5
             var base = m.cleanContent.replace(`${prefix}giveaway `, "").split(" | ");
             if (!isNaN(+base[1])) {
@@ -17,42 +20,60 @@ module.exports = {
             if (m.content == `${prefix}giveaway`) {
                 return resolve(Bot.createMessage(m.channel.id, "Please add something to giveaway. \n\ni.e. `!giveaway Dragon Dildos` for giving away 'Dragon Dildos'\nor `!giveaway Dragon Dildos | 1` for giving away 'Dragon Dildos' in 1 hour (time defaults to 0.5 hours)"));
             } else {
+                if (!(data[guild.id])) {
+                    data[guild.id] = {}
+                    data[guild.id].name = guild.name
+                    data[guild.id].owner = guild.ownerID
+                    Bot.createMessage(m.channel.id, `Server: ${guild.name} added to database. Populating information ${hand}`).then((msg) => {
+                        return setTimeout(function() {
+                            Bot.deleteMessage(m.channel.id, m.id, "Timeout")
+                            Bot.deleteMessage(m.channel.id, msg.id, "Timeout")
+                        }, 5000)
+                    })
+                    _.save(data)
+                    _.load()
+                }
                 var author = m.author.id;
                 var name = m.channel.guild.members.get(author).nick || m.channel.guild.members.get(author).username;
-                if (data.giveaways.running) {
+                if (!data[guild.id].giveaways) {
+                  data[guild.id].giveaways = {}
+                  _.save(data)
+                  data = _.load()
+                }
+                if (data[guild.id].giveaways.running) {
                     Bot.createMessage(m.channel.id, "There is already a giveaway running, try again later~");
                     return;
                 }
-                if (!data.giveaways.running) {
-                    data.giveaways.running = true;
-                    data.giveaways.item = msg;
-                    data.giveaways.current = {};
-                    data.giveaways.creator = author;
-                    data.giveaways.current.contestants = {};
+                if (!data[guild.id].giveaways.running) {
+                    data[guild.id].giveaways.running = true;
+                    data[guild.id].giveaways.item = msg;
+                    data[guild.id].giveaways.current = {};
+                    data[guild.id].giveaways.creator = author;
+                    data[guild.id].giveaways.current.contestants = {};
                     Bot.createMessage(m.channel.id, `***${name}*** has started a giveaway for: **` + msg + `**. React to this message with <:giveaway:367892951780818946> in ${time} hour(s) to enter!`).then((m) => {
-                        data.giveaways.mID = m.id;
-                        data.giveaways.channelID = m.channel.id;
+                        data[guild.id].giveaways.mID = m.id;
+                        data[guild.id].giveaways.channelID = m.channel.id;
                         _.save(data);
-                        Bot.addMessageReaction(data.giveaways.channelID, data.giveaways.mID, "giveaway:367892951780818946")
+                        Bot.addMessageReaction(data[guild.id].giveaways.channelID, data[guild.id].giveaways.mID, "giveaway:367892951780818946")
                         var amount = time * 3600000
                         return promiseTimeout(amount);
                     }).then(() => {
                         const data = _.load();
                         var contestants = [];
-                        var entries = data.giveaways.current.contestants;
+                        var entries = data[guild.id].giveaways.current.contestants;
                         Object.keys(entries).forEach(function(key) {
                             contestants.push(key);
                         });
                         var winnerID = contestants[Math.floor(Math.random() * contestants.length)];
                         if (winnerID == undefined) {
-                            data.giveaways.running = false;
+                            data[guild.id].giveaways.running = false;
                             _.save(data);
-                            return Bot.editMessage(data.giveaways.channelID, data.giveaways.mID, ` This Giveaway has ended, no one entered. So I guess everyone loses?`);
+                            return Bot.editMessage(data[guild.id].giveaways.channelID, data[guild.id].giveaways.mID, `This Giveaway has ended, no one entered. So I guess everyone loses?`);
                         }
                         var winner = m.channel.guild.members.get(winnerID).mention
-                        data.giveaways.running = false;
+                        data[guild.id].giveaways.running = false;
                         _.save(data);
-                        return Bot.editMessage(data.giveaways.channelID, data.giveaways.mID, ` This Giveaway has ended~`) && Bot.createMessage(m.channel.id, `Congrats ${winner}! :tada: :tada: :tada:`);
+                        return Bot.editMessage(data[guild.id].giveaways.channelID, data[guild.id].giveaways.mID, "This Giveaway has ended") && Bot.createMessage(m.channel.id, `Congrats ${winner}! :tada: :tada: :tada:\nYou won <@${data[guild.id].giveaways.creator}>'s giveaway for: \`${data[guild.id].giveaways.item}\``);
                     }).catch(console.log);
                 }
             }
