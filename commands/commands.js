449a3612c88f79ef6eb7d580c19a6033cca6397e
LaChocola <thenekoluv@gmail.com>
2018-10-30 17:56:17 -0500

    fixed display issues

--- commands/commands.js
+++ commands/commands.js
@@ -1,29 +1,29 @@
-const fs = require("fs");
+const fs = require('fs');
 
 module.exports = {
-    main: function(Bot, m, args, prefix) {
-        function format(file, help) {
-            var line = "`"+ prefix + file.replace(".js", "") + "` " + help + ".";
-            return line;
-        }
-        var files = fs.readdirSync("./commands/");
-        var lines = [];
-        files.forEach(function(file) {
-            if (lines.length < 1800) {
-                var cmd = require("./" + file)
-                if (!cmd.hidden) {
-                  lines.push(format(file, cmd.help));
-                }
-            }
-        });
-        var message = lines.join("\n");
-        Bot.createMessage(m.channel.id, {
-            content: `Here are the **commands** you can use`,
-            embed: {
-                color: 0x5A459C,
-                description: message
-            }
-        });
-    },
-    help: "List of commands"
-}
+	main(Bot, m, args, prefix) {
+		function format(file, help) {
+			const line = '`' + prefix + file.replace('.js', '') + '` ' + help + '.';
+			return line;
+		}
+		const files = fs.readdirSync('./commands/');
+		const lines = [];
+		files.forEach(file => {
+			if (lines.length < 1800) {
+				const cmd = require('./' + file);
+				if (!cmd.hidden) {
+					lines.push(format(file, cmd.help));
+				}
+			}
+		});
+		const message = lines.join('\n');
+		Bot.createMessage(m.channel.id, {
+			content: 'Here are the **commands** you can use',
+			embed: {
+				color: 0x5A459C,
+				description: message
+			}
+		});
+	},
+	help: 'List of commands'
+};
