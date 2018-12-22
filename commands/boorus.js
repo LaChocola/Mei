50f6bd13cc078522ff64af08e10f531c6088d5c4
LaChocola <thenekoluv@gmail.com>
2018-12-22 07:16:56 -0600

    Removed boorus command. Use !booru list

--- commands/boorus.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	main(Bot, m) {
-		Bot.createMessage(m.channel.id, '**Total Boorus Availible:** 16\n**Default Booru:** giantessbooru.com \n\n**Other Availbe Boorus | Aliases:**\n***giantessbooru*** | gtsbooru, gbooru\n***e621*** | e6, e621, e9, e926\n***hypnohub*** | hh, hypno\n***danbooru*** | db\n***konachan*** | kc, konan\n***yande.re*** | yd, yand\n***gelbooru*** | gb, gel\n***rule34*** | r34\n***safebooru*** | sb\n***thebigimagebooru*** | tb, tbib\n***xbooru*** | xb\n***youhate*** | yh\n***dollbooru***\n***derpibooru*** | dp, derpi\n***furrybooru*** | fb \n***realbooru***');
-	},
-	help: 'Boorus availible in ``!booru``'
-};
