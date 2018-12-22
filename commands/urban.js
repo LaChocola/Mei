8863eb3573cc9010f9e2e653c0ba06b2b506f281
LaChocola <thenekoluv@gmail.com>
2018-12-22 07:15:04 -0600

    Improved functionality and made more es lint compatible

--- commands/urban.js
+++ commands/urban.js
@@ -2,30 +2,34 @@ const request = require('request');
 const esc = require('url-escape-tag');
 
 module.exports = {
-    main: function(Bot, m, args, prefix) {
-        var word = (m.content.replace(`${prefix}urban `, ""));
-        var urbanJsonURL = "http://api.urbandictionary.com/v0/define?term=" + esc`${word}`
-        var urbanJson = request.get({
+    main: function (Bot, m, args, prefix) {
+        var word = (m.content.replace(`${prefix}urban `, ''));
+        var urbanJsonURL = 'http://api.urbandictionary.com/v0/define?term=' + esc`${word}`;
+        request.get({
             url: urbanJsonURL,
-            json: true,
-        }, function(e, r, b) {
-            if (!e && b.list[0] != undefined) {
+            json: true
+        }, function (e, r, b) {
+            if (!e && b.list[0] !== undefined) {
                 var message = [
-                    "```" + b.list[0].definition + "```",
-                    "Example: " + b.list[0].example,
-                    "<" + b.list[0].permalink + ">"
-                ].join("\n")
+                    '```' + b.list[0].definition + '```',
+                    'Example: ' + b.list[0].example,
+                    '<' + b.list[0].permalink + '>'
+                ].join('\n');
+                if (message.length > 2000) {
+                    Bot.createMessage(m.channel.id, `Sorry, the definition of **${b.list[0].word}** is too long to post.`);
+                    return;
+                }
                 Bot.createMessage(m.channel.id, {
-                    content: "Urban Dictionary definition of **" + b.list[0].word + "**:\n",
+                    content: `Urban Dictionary definition of **${b.list[0].word}**:\n`,
                     embed: {
                         color: 0xA260F6,
                         description: message
                     }
                 });
             } else {
-                Bot.createMessage(m.channel.id, "Not found");
-            };
+                Bot.createMessage(m.channel.id, 'Not found');
+            }
         });
     },
-    help: "Search Urban Dictionary" // add description
-}
+    help: 'Search Urban Dictionary'
+};
