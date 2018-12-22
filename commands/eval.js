73ec7f8df8d3688d590416be54166de551d888ee
LaChocola <thenekoluv@gmail.com>
2018-12-22 07:13:01 -0600

    Upgraded to promise based eval

--- commands/eval.js
+++ commands/eval.js
@@ -1,21 +1,21 @@
 const util = require("util");
 
 module.exports = {
-    main: function(Bot, m, args, prefix) {
+    main: async function(Bot, m, args, prefix) {
         var coolkids = ["161027274764713984", "176975815072808960", "147383057844797441", "103832588556193792", "196026737019191296"];
         if (coolkids.indexOf(m.author.id) > -1) {
-            var spoopy = "weeb";
             var u;
             if (args.indexOf("--util") > -1) {
                 u = true;
                 args = args.replace("--util", "");
             }
             try {
-                var ev = eval(args);
+                var ev = await eval(args);
                 if (u) {
                     ev = util.inspect(ev)
                 }
                 Bot.createMessage(m.channel.id, ev);
+                console.log(ev);
             } catch (err) {
                 Bot.createMessage(m.channel.id, err);
             }
