a56638d65ef5b35e30207333dd21ef65b3a9a4da
LaChocola <thenekoluv@gmail.com>
2019-01-29 20:04:31 -0600

    made eslint compatible

--- commands/roles.js
+++ commands/roles.js
@@ -1,42 +1,35 @@
 module.exports = {
-    main: function(Bot, m, args, prefix) {
-        var guild = m.channel.guild
-        var name1 = m.cleanContent.replace(`${prefix}roles `, "")
+    main: function (Bot, m, args, prefix) {
+        var guild = m.channel.guild;
+        var name1 = m.cleanContent.replace(`${prefix}roles `, '');
         if (m.content.length < 7) {
-            name1 = m.author.username
+            name1 = m.author.username;
         }
-        var isThisUsernameThatUsername = function(member) {
-            var memberName = member.nick || member.username
-            if (memberName.toLowerCase() == name1.toLowerCase()) {
+        var isThisUsernameThatUsername = function (member) {
+            var memberName = member.nick || member.username;
+            if (memberName.toLowerCase() === name1.toLowerCase()) {
                 return true;
             }
-        }
+        };
 
-        var member = m.guild.members.find(isThisUsernameThatUsername)
-        if (m.mentions.length < 1) {
-            var mentioned = member || guild.members.get(m.author.id)
+        var member = m.guild.members.find(isThisUsernameThatUsername);
+        if (m.mentions.length === 0) {
+            var mentioned = member || guild.members.get(m.author.id);
         }
         if (m.mentions.length > 0) {
-            var mentioned = guild.members.get(m.mentions[0].id)
+            var mentioned = guild.members.get(m.mentions[0].id);
         }
         var arrayOfRoleIDs = mentioned.roles;
-        var getRoleObjectFromRoleID = function(roleID) {
-            return guild.roles.get(roleID);
-        }
-        var arrayOfRoleObjects = arrayOfRoleIDs.map(getRoleObjectFromRoleID);
-        var getRoleNameFromRoleID = function(roleID) {
+        var getRoleNameFromRoleID = function (roleID) {
             return guild.roles.get(roleID).name;
-        }
-        var arrayOfRoleNames = arrayOfRoleIDs.map(getRoleNameFromRoleID);
-        for (let role of arrayOfRoleObjects) {
-            console.log("\n" + role.name.toLowerCase() + " (" + role.id + ")");
-        }
-        var msg = JSON.stringify(mentioned.permission.json).replace("{", "").replace("}", "")
-        if (arrayOfRoleNames.length < 1) {
-          Bot.createMessage(m.channel.id, "You do not currently have any assigned roles in this server.");
-          return;
         };
-        Bot.createMessage(m.channel.id, "Roles for: **" + mentioned.username + "#" + mentioned.discriminator + "**\n \n*" + arrayOfRoleNames.join("*, *") + "*\n \nPermissions: ```js\n" + msg + "```")
+        var arrayOfRoleNames = arrayOfRoleIDs.map(getRoleNameFromRoleID);
+        var msg = JSON.stringify(mentioned.permission.json).replace('{', '').replace('}', '');
+        if (arrayOfRoleNames.length === 0) {
+          Bot.createMessage(m.channel.id, 'You do not currently have any assigned roles in this server.');
+          return;
+        }
+        Bot.createMessage(m.channel.id, 'Roles for: **' + mentioned.username + '#' + mentioned.discriminator + '**\n \n*' + arrayOfRoleNames.join('*, *') + '*\n \nPermissions: ```js\n' + msg + '```');
     },
-    help: "Shows role info on mentioned person, use !role to assign roles"
-}
+    help: 'Shows role info on mentioned person, use !role to assign roles'
+};
