a648ad85fec3f98549989fa9c81647330da80818
Natalie Fearnley <nfearnley@gmail.com>
2019-08-30 04:18:14 -0400

    Rewrote date

--- commands/date.js
+++ commands/date.js
@@ -3,60 +3,54 @@
 // TODO: Replace this with moment
 const timeago = require("timeago.js");
 const timediff = require("timediff");
+const moment = require("require");
+const escapeStringRegexp = require("escape-string-regexp");
 
 const utils = require("../utils");
 
+function isNum(val) {
+    return Boolean(val && !isNaN(+val));
+}
+
+function getArgs(msg, cmd, prefix) {
+    var commandRegex = RegExp(`^${escapeStringRegexp(prefix)}${escapeStringRegexp(cmd)}`, "i");
+    var commandArgs = msg.replace(commandRegex, "").trim();
+    return commandArgs;
+}
+
+function getTagged(args) {
+    var tagMatch = args.match(/<@(.*)>/);
+    var taggedId = tagMatch & isNum(tagMatch[0]) && tagMatch[0];
+    return taggedId;
+}
+
 module.exports = {
     main: function(Bot, m, args, prefix) {
-        var name1 = m.cleanContent.replace(/!date /i, "");
+        var commandArgs = getArgs(m.cleanContent, "date", prefix);
 
-        var member = m.guild.members.find(m => utils.isSameMember(m, name1));
-        var mentioned = m.mentions[0] || member || m.author;
-        var id = undefined;
-        var name = undefined;
-        if (mentioned) {
-            id = mentioned.id;
-            name = mentioned.username;
-        }
-        if (!id) {
-            var args2 = m.cleanContent.replace("!date ", "").replace("<@", "").replace(">", "").trim();
-            if (!isNaN(+args2)) {
-                id = args2;
-            }
-        }
+        var taggedId = getTagged(commandArgs);
+        var member = m.guild.members.find(m => utils.isSameMember(m, commandArgs));
+        var mentionedMember = m.mentions[0] || member || m.author;
+        var mentionedId = (mentionedMember && mentionedMember.id) || taggedId;
 
-        mentioned = m.guild.members.find(m => utils.isSameMember(m, name1));
-        member = m.mentions[0] || mentioned || m.author;
-        args2 = m.content.replace("!date ", "").replace("<@", "").replace(">", "").trim();
-        if (args2.length > 1) {
-            id = args2;
-        }
-        if (id != undefined) {
-            member = m.channel.guild.members.get(id);
-        }
-        else if (!id) {
-            member = m.channel.guild.members.get(member.id);
-        }
+        member = m.channel.guild.members.get(mentionedId);
+
         if (!member) {
-            Bot.createMessage(m.channel.id, "I could not find that member or id in this server").then((msg) => {
-                return setTimeout(function() {
-                    Bot.deleteMessage(m.channel.id, msg.id, "Timeout");
-                    Bot.deleteMessage(m.channel.id, m.id, "Timeout");
-                }, 5000);
-            });
+            m.reply("I could not find that member or id in this server", 5000);
             return;
         }
-        var date = member.joinedAt;
-        var date2 = member.createdAt;
-        name = member.nick || member.username;
-        var length = new Date(date).toDateString();
-        var length2 = new Date(date2).toDateString();
-        var ago = timeago.format(date);
-        var ago2 = timeago.format(date2);
-        var diff = timediff(date2, date, "D");
-        Bot.createMessage(m.channel.id, "**" + name + "**\nJoined: " + length + " | " + ago + "\nCreated: " + length2 + " | " + ago2);
-        if (diff.days < 2) {
-            Bot.createMessage(m.channel.id, ":warning: **" + name + "** Joined less than 24 hours after creating account");
+
+        var joinedDate = moment(member.joinedAt);
+        var createdDate = moment(member.createdAt);
+        var name = member.nick || member.username;
+        var joinedString = joinedDate.format("ddd MMM DD YYYY");
+        var createdString = createdDate.format("ddd MMM DD YYYY");
+        var sinceJoined = joinedDate.fromNow();
+        var sinceCreated = createdDate.fromNow();
+        var diffDays = createdDate.diff(joinedDate, "days");
+        m.reply("**" + name + "**\nJoined: " + joinedString + " | " + sinceJoined + "\nCreated: " + createdString + " | " + sinceCreated);
+        if (diffDays < 2) {
+            m.reply(":warning: **" + name + "** Joined less than 24 hours after creating account");
         }
     },
     help: "Date a user joined the server"
