5b36376fc7fd4688d476fa09aa50c733f4ea3e77
Natalie Fearnley <nfearnley@gmail.com>
2019-09-02 00:47:17 -0400

    Updated art command

--- commands/art.js
+++ commands/art.js
@@ -1,146 +1,125 @@
 "use strict";
 
+const moment = require("moment");
+
 const utils = require("../utils");
 const dbs = require("../dbs");
 
+function addArt(art, url, msg, isImage) {
+    art[url] = {
+        url: url,
+        authorId: msg.author.id,
+        timestamp: msg.timestamp,
+        isImage: isImage
+    };
+}
+
+function pick(options) {
+    return options[Math.floor(Math.random() * options.length)];
+}
+
 module.exports = {
-    main: function(Bot, m, args, prefix) {
+    main: async function(bot, m, args, prefix) {
         var guildDb = dbs.guild.load();
 
         var guild = m.channel.guild;
         if (!guildDb[guild.id]) {
-            guildDb[guild.id] = {};
-            guildDb[guild.id].name = guild.name;
-            guildDb[guild.id].owner = guild.ownerID;
-            Bot.createMessage(m.channel.id, `Server: ${guild.name} added to database. Populating information ${utils.hands.ok()}`).then((msg) => {
-                return setTimeout(function() {
-                    Bot.deleteMessage(m.channel.id, m.id, "Timeout");
-                    Bot.deleteMessage(m.channel.id, msg.id, "Timeout");
-                }, 5000);
-            });
+            guildDb[guild.id] = {
+                name: guild.name,
+                owner: guild.ownerID
+            };
+            m.reply(`Server: ${guild.name} added to database. Populating information ${utils.hands.ok()}`, 5000);
             dbs.guild.save(guildDb);
         }
-        if (!guildDb[guild.id].art) {
-            Bot.createMessage(m.channel.id, `An art channel has not been set up for this server. Please have a mod add one using the command: \`${prefix}edit art add #channel\``).then((msg) => {
-                return setTimeout(function() {
-                    Bot.deleteMessage(m.channel.id, msg.id, "Timeout");
-                    Bot.deleteMessage(m.channel.id, m.id, "Timeout");
-                }, 10000);
-            });
+        var guildData = guildDb[guild.id];
+
+        var artChannelId = guildData.art;
+
+        if (!artChannelId) {
+            m.reply(`An art channel has not been set up for this server. Please have a mod add one using the command: \`${prefix}edit art add #channel\``, 10000);
+            m.deleteIn(10000);
             return;
         }
-        var index = 5000;
-        if (args) {
-            if (!isNaN(+args)) {
-                index = args;
-            }
+
+        var limit = 5000;
+        if (args && !isNaN(+args)) {
+            limit = parseInt(args, 10);
         }
-        var channel = guildDb[guild.id].art;
-        channel = Bot.getChannel(channel);
-        console.log(channel.nsfw);
+        var artChannel = bot.getChannel(artChannelId);
+
+        console.log(artChannel.nsfw);
         console.log(m.channel.nsfw);
-        var cName = channel.name;
-        var gName = channel.guild.name;
-        var icon = channel.guild.iconURL || null;
-        if (channel.nsfw && !m.channel.nsfw) {
-            Bot.createMessage(m.channel.id, `The selected art channel, <#${channel.id}>, is an nsfw channel, and this channel is not. Please either use this command in an nsfw channel, or set the art channel to a non-nsfw channel`).then((msg) => {
-                return setTimeout(function() {
-                    Bot.deleteMessage(m.channel.id, msg.id, "Timeout");
-                    Bot.deleteMessage(m.channel.id, m.id, "Timeout");
-                }, 10000);
-            });
+
+        var icon = artChannel.guild.iconURL || null;
+
+        if (artChannel.nsfw && !m.channel.nsfw) {
+            m.reply(`The selected art channel, <#${artChannel.id}>, is an nsfw channel, and this channel is not. Please either use this command in an nsfw channel, or set the art channel to a non-nsfw channel`, 10000);
+            m.deleteIn(10000);
             return;
         }
-        channel = channel.id;
-        if (index > 7000) {
-            Bot.createMessage(m.channel.id, "I can't grab more than 7000 messages in any channel. Setting limit to 7000").then((msg) => {
-                return setTimeout(function() {
-                    Bot.deleteMessage(m.channel.id, msg.id, "Timeout");
-                }, 5000);
-            });
-            index = 7000;
+
+        if (limit > 7000) {
+            m.reply("I can't grab more than 7000 messages in any channel. Setting limit to 7000.", 5000);
+            limit = 7000;
         }
-        Bot.sendChannelTyping(m.channel.id).then(async () => {
-            Bot.getMessages(channel, parseInt(index)).then(function(msgs) {
-                var art = {};
-                for (var msg of msgs) {
-                    if (msg.content.includes("pastebin.com")) {
-                        art[msg.content] = [msg.author.id, msg.timestamp];
-                    }
-                    if (msg.attachments[0]) {
-                        art[msg.attachments[0].url] = [msg.author.id, msg.timestamp];
-                    }
-                    if (msg.embeds[0]) {
-                        if (msg.embeds[0].image) {
-                            art[msg.embeds[0].image.url] = [msg.author.id, msg.timestamp];
-                        }
-                        if (!msg.embeds[0].image) {
-                            art[msg.embeds[0].url] = [msg.author.id, msg.timestamp];
-                        }
-                    }
-                }
-                var number = Math.floor(Math.random() * Object.entries(art).length);
-                var list = Object.entries(art);
-                var chosen = list[number];
-                console.log(chosen);
-                if (!chosen) {
-                    Bot.createMessage(m.channel.id, `No art was found within the last \`${index}\` messages. Please try again using more messages`).then((msg) => {
-                        return setTimeout(function() {
-                            Bot.deleteMessage(m.channel.id, msg.id, "Timeout");
-                            Bot.deleteMessage(m.channel.id, m.id, "Timeout");
-                        }, 5000);
-                    });
-                    return;
-                }
-                var author = m.channel.guild.members.get(chosen[1][0]) || m.channel.guild.members.get(chosen[1][0]) || Bot.users.get(chosen[1][0]);
-                var url = author.avatarURL || undefined;
-                author = author.nick || author.username;
-                var time = new Date(chosen[1][1]).toISOString();
-                if (chosen[0].includes("pastebin.com")) {
-                    const data = {
-                        "embed": {
-                            "color": 0xA260F6,
-                            "title": chosen[0],
-                            "description": `A random piece from <#${channel}>~`,
-                            "url": chosen[0],
-                            "timestamp": time,
-                            "author": {
-                                "name": author,
-                                "icon_url": url
-                            },
-                            "footer": {
-                                "icon_url": icon,
-                                "text": `${cName} | ${gName}`
-                            }
-                        }
-                    };
-                    Bot.createMessage(m.channel.id, data);
-                    return;
+
+        await m.sendTyping();
+        var msgs = await artChannel.getMessages(limit);
+
+        var artDict = {};
+        for (var msg of msgs) {
+            if (msg.content.includes("pastebin.com")) {
+                addArt(artDict, msg.content, msg, false);
+            }
+            if (msg.attachments[0]) {
+                addArt(artDict, msg.attachments[0].url, msg, true);
+            }
+            if (msg.embeds[0]) {
+                if (msg.embeds[0].image) {
+                    addArt(artDict, msg.embeds[0].image.url, msg, true);
                 }
                 else {
-                    const data = {
-                        "embed": {
-                            "color": 0xA260F6,
-                            "timestamp": time,
-                            "description": `A random piece from <#${channel}>~`,
-                            "image": {
-                                "url": chosen[0]
-                            },
-                            "author": {
-                                "name": author,
-                                "icon_url": url
-                            },
-                            "footer": {
-                                "icon_url": icon,
-                                "text": `${cName} | ${gName}`
-                            }
-                        }
-                    };
-                    Bot.createMessage(m.channel.id, data);
+                    addArt(artDict, msg.embeds[0].url, msg, true);
                 }
-            });
-        });
-        return;
+            }
+        }
+
+        var art = pick(Object.values(artDict));
+
+        console.log(art);
+        if (!art) {
+            m.reply(`No art was found within the last \`${limit}\` messages. Please try again using more messages`, 5000);
+            m.deleteIn(5000);
+            return;
+        }
+
+        var author = m.channel.guild.members.get(art.authorId) || m.channel.guild.members.get(art.authorId) || bot.users.get(art.authorId);
+        var authorName = author.nick || author.username;
+        var authorIcon = author.avatarURL || undefined;
+        var time = moment(art.timestamp).format();
+        var embed = {
+            color: 0xA260F6,
+            timestamp: time,
+            description: `A random piece from <#${artChannel.id}>~`,
+            author: {
+                name: authorName,
+                icon_url: authorIcon
+            },
+            footer: {
+                icon_url: icon,
+                text: `${artChannel.name} | ${artChannel.guild.name}`
+            }
+        };
+        if (art.isImage) {
+            embed.image = {
+                url: art.url
+            };
+        }
+        else {
+            embed.title = art.url;
+            embed.url = art.url;
+        }
+        m.reply({ embed: embed });
     },
     help: "Show art from the set art channel" // add description
 };
