a1cba58ff8524480caf02dad5c9a72046f667774
Natalie Fearnley <nfearnley@gmail.com>
2019-09-01 22:45:12 -0400

    Updated allroles command

--- commands/allroles.js
+++ commands/allroles.js
@@ -1,13 +1,14 @@
 "use strict";
 
 module.exports = {
-    main: function(Bot, m, args, prefix) {
+    main: function(bot, m, args, prefix) {
         var roles = m.guild.roles;
-        var roleNames = roles.map(r => r.name).join("  |  ");
-        var roleDumps = roles.map(r => `"${r.name}": "${r.id}",`).join("\n");
 
-        console.log(roleDumps);
-        Bot.createMessage(m.channel.id, roleNames);
+        var roleDump = roles.map(r => `"${r.name}": "${r.id}",`).join("\n");
+        console.log(roleDump);
+
+        var roleNames = roles.map(r => r.name).join("  |  ");
+        m.reply(roleNames);
     },
     help: "Role list"
 };
