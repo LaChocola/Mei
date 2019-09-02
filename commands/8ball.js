ae1efcfcab2c94918be9c4a4aeffc1d0e8c422a7
Natalie Fearnley <nfearnley@gmail.com>
2019-09-01 22:33:35 -0400

    minor fix

--- commands/8ball.js
+++ commands/8ball.js
@@ -5,7 +5,7 @@ const eightball = require("8ball")();
 module.exports = {
     main: function(Bot, m, args, prefix) {
         var msg = m.cleanContent.replace(`${prefix}8ball `, "");
-        if (m.content == `${prefix}8ball`) {
+        if (m.content === `${prefix}8ball`) {
             Bot.createMessage(m.channel.id, "Please add something");
         }
         else {
