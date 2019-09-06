72dfa5c8bab57d74626db1cf545e5c67f685a30d
Natalie Fearnley <nfearnley@gmail.com>
2019-09-06 00:57:33 -0400

    created Member.isMod

--- botplus.js
+++ botplus.js
@@ -1,6 +1,7 @@
 ﻿"use strict";
 
 const utils = require("./utils");
+const dbs = require("./dbs");
 
 function extend(bot) {
     Object.defineProperty(bot.Message.prototype, "guild", {
@@ -44,6 +45,49 @@ function extend(bot) {
             });
 
             return m;
+        }
+    });
+
+    Object.defineProperty(bot.Member.prototype, "isMod", {
+        value: async function() {
+            var member = this;
+            var guild = member.guild;
+            var guildDb = await dbs.guild.load();
+            var guildData = guildDb[guild.id] || {};
+
+            // Check if member is the guild owner
+            var isOwner = member.id === guild.ownerID;
+            if (isOwner) {
+                return true;
+            }
+
+            // Check if member is in the list of guild mods
+            var isMod = guildData.mods && guildData.mods[member.id];
+            if (isMod) {
+                return true;
+            }
+
+            // Check if member has one of the mod roles
+            var modRoles = guildData.modRoles && Object.keys(guildData.modRoles) || [];
+            var hasModRole = modRoles.some(r => member.roles.includes(r));
+            if (hasModRole) {
+                return true;
+            }
+
+            return false;
+        }
+    });
+
+    //member.hasPerms(["banMembers", "administrator", "manageGuild", "manageChannels", "manageMessages"]);  // "!clean" command
+    //member.hasPerms(["banMembers", "administrator", "manageGuild", "manageChannels"]);                    // "!edit" command
+    //member.hasPerms(["banMembers", "administrator", "manageGuild"]);                                      // "!ban" command
+    Object.defineProperty(bot.Member.prototype, "hasPerms", {
+        get: async function(perms) {
+            var member = this;
+            // Make a Set() of all member permissions that are set to true
+            var memberPerms = new Set(Object.keys(member.permission.json.filter(p => p)));
+            var hasPerm = perms.some(p => memberPerms.has(p));
+            return hasPerm;
         }
     });
 }
