95404756a90d7c0d9a48e45171adc20f437c44ec
Natalie Fearnley <nfearnley@gmail.com>
2019-09-12 03:30:28 -0400

    cleaned up wish

--- commands/wish.js
+++ commands/wish.js
@@ -3,6 +3,7 @@
 // This version of Jimp has an alphabet I created to emulated the DeviantArt username font "Trebuchet" and timestamp font
 // They are both availible at https://github.com/LaChocola/Mei/tree/master/data/fonts
 const Jimp = require("jimp");
+const moment = require("moment");
 
 const utils = require("../utils");
 
@@ -11,49 +12,38 @@ var timeFontPath = utils.getPkgPath("/data/fonts/timefont/timeFont.fnt");       
 
 module.exports = {
     main: async function(bot, m, args, prefix) {
-        var time = new Date().toDateString().slice(4).replace(` ${new Date().getFullYear()}`, `, ${new Date().getFullYear()}`);
-
-        var member = m.guild.members.find(m => utils.isSameMember(m, m.author));
-        var mentioned = m.mentions[0] || member || m.author;
-        var name = m.channel.guild.members.get(mentioned.id).nick || mentioned.username;
-        if (name.length > 13) {
-            name = name.slice(0, 11) + "..";
-        }
-        var pic = `https://images.discordapp.net/avatars/${m.author.id}/${m.author.avatar}.png?size=1024`;
-        if (pic.includes("null")) {
-            m.reply("You need an avatar to use this command");
-            return;
-        }
-        if (m.mentions.length == 1) {
-            pic = `https://images.discordapp.net/avatars/${m.mentions[0].id}/${m.mentions[0].avatar}.png?size=1024`;
-        }
-        else if (m.mentions.length > 1) {
+        if (m.mentions.length > 1) {
             m.reply("This Command cant be used with more than one mention");
             return;
         }
-        m.channel.sendTyping().then(async () => {
-            try {
-                const bg = await Jimp.read("https://buttsare.sexy/4Vp1MUG.png");
-                const avy = await Jimp.read(pic);
-                const nameFont = await Jimp.loadFont(nameFontPath);
-                const timeFont = await Jimp.loadFont(timeFontPath);
-                avy.resize(141, 116);
-                bg.clone()
-                    .blit(avy, 15, 12)
-                    .print(nameFont, 215, 30, name)
-                    .print(timeFont, 460, 37, time)
-                    .getBuffer(Jimp.MIME_PNG, function(err, buffer) {
-                        m.reply("", {
-                            "file": buffer,
-                            "name": "wish.png"
-                        });
-                    });
-            }
-            catch (error) {
-                console.log(error);
-                return m.reply("Something went wrong...");
-            }
-        });
+
+        var time = moment().format("MMM D, YYYY");
+
+        var member = m.guild.members.get(m.mentions[0] && m.mentions[0].id) || m.member;
+        var name = member.name;
+        if (name.length > 13) {
+            name = name.slice(0, 11) + "..";
+        }
+        var avatarUrl = `https://images.discordapp.net/avatars/${member.id}/${member.avatar || member.defaultAvatar}.png?size=1024`;
+        var templateUrl = "https://buttsare.sexy/4Vp1MUG.png";
+
+        await m.channel.sendTyping();
+        const bg = await Jimp.read(templateUrl);
+        const avy = await Jimp.read(avatarUrl);
+        const nameFont = await Jimp.loadFont(nameFontPath);
+        const timeFont = await Jimp.loadFont(timeFontPath);
+        avy.resize(141, 116);
+        bg.clone()
+            .blit(avy, 15, 12)
+            .print(nameFont, 215, 30, name)
+            .print(timeFont, 460, 37, time)
+            .getBuffer(Jimp.MIME_PNG, function(err, buffer) {
+                m.reply({
+                    file: buffer,
+                    name: "wish.png"
+                });
+            });
+
     },
     help: ";)"
 };
