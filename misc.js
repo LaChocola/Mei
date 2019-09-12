24c0c6dd7d153078ceb1c3044e139af205234889
Natalie Fearnley <nfearnley@gmail.com>
2019-09-12 00:24:57 -0400

    removed unused file

--- misc.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-class Misc { // Declaring export as a class because cbf to make other way work properly. Should probably do other way for consistancy though
-    
-}
-
-module.exports = Misc;
