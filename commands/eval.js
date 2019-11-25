60382c0487616c3ce19cc27e5ae8c74d5e0fd4bc
LaChocola <thenekoluv@gmail.com>
2019-11-25 07:51:20 -0600

    Fixed Sandboxing for authorized users

--- commands/eval.js
+++ commands/eval.js
@@ -2,7 +2,18 @@ var safeEval = require('safe-eval')
 
 module.exports = {
     main: async function(Bot, m, args, prefix) {
-        var coolkids = ["161027274764713984", "271803699095928832", "137269976255037440"];
+        var coolkids = ["176975815072808960", "147383057844797441", "103832588556193792", "196026737019191296"];
+        if (m.author.id == "161027274764713984") {
+            try {
+                var ev = eval(args);
+                var ev = await eval(args);
+                Bot.createMessage(m.channel.id, ev);
+                console.log(ev);
+            } catch (err) {
+                Bot.createMessage(m.channel.id, err);
+            }
+            return;
+        }
         if (coolkids.indexOf(m.author.id) > -1) {
             try {
                 var ev = await safeEval(args);
@@ -11,8 +22,18 @@ module.exports = {
             } catch (err) {
                 Bot.createMessage(m.channel.id, err);
             }
-        } else {
-            Bot.createMessage(m.channel.id, "No, dont touch that");
+            return;
+        }
+        if (!coolkids.indexOf(m.author.id) > -1) {
+            var responses = ["Are you a real villan?", "Have you ever caught a good guy? \nLike a real super hero?", "Have you ever tried a disguise?", "What are you doing?!?!?!", "*NO!*, Don't touch that!", "Fuck Off", "Roses are red\nfuck me ;) "]
+            var response = responses[Math.floor(Math.random() * responses.length)]
+            Bot.createMessage(m.channel.id, response).then((msg) => {
+                return setTimeout(function() {
+                    Bot.deleteMessage(m.channel.id, msg.id, "Timeout")
+                    Bot.deleteMessage(m.channel.id, m.id, "Timeout")
+                }, 5000)
+              })
+              return;
         }
     },
     help: "Just dont",
