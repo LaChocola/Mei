54223666e01eade7419ba331d43f98c362bbdda3
Natalie Fearnley <nfearnley@gmail.com>
2020-01-22 15:59:56 -0500

    cleaned up db code

---
+++ dbmanager.js
@@ -0,0 +1,64 @@
+"use strict";
+
+const fs = require("fs").promises;
+const ids = require("./ids");
+
+const dbprefix = "./db/";
+const backupprefix = "../../backup/Mei/db/";
+
+function getdbpath(dbname) {
+    return `${dbprefix}${dbname}.json`;
+}
+
+function getbackuppath(dbname) {
+    return `${backupprefix}${dbname}.json`;
+}
+
+// Try loading a file from a path. Return undefined on failure.
+async function loadFrom(path) {
+    var data = undefined;
+    try {
+        var rawdata = await fs.readFile(path, "utf8");
+        if (!rawdata.includes(ids.users.chocola)) {
+            throw "db does not include chocola id";
+        }
+        data = JSON.parse(rawdata);
+    }
+    catch (err) {
+        console.error(`Error loading ${path}: ${err}`);
+    }
+    return data;
+}
+
+// Load a db by name
+async function load(dbname) {
+    var dbpath = getdbpath(dbname);
+    var backupath = getbackuppath(dbname);
+    var data = loadFrom(dbpath);
+    // If we failed to load the db, try loading and saving the backup copy
+    if (!data) {
+        console.log("JSON error, attempting restore");
+        data = loadFrom(backupath);
+        // If we successfully loaded the backup, save it
+        if (data) {
+            save(data);
+            console.log("Restore Successful");
+        }
+    }
+    // If we weren't able to load the data OR the backup, then throw an error
+    if (!data) {
+        throw "Unable to load db";
+    }
+    return data;
+}
+
+// Save a db by name
+async function save(dbname, data) {
+    var dbpath = getdbpath(dbname);
+    await fs.writeFile(dbpath, JSON.stringify(data, null, "\t"));
+}
+
+module.exports = {
+    load,
+    save
+};
--- people.js
+++ people.js
@@ -1,33 +1,18 @@
 "use strict";
 
-const fs = require("fs").promises;
-const ids = require("./ids");
+const dbmanager = require("./dbmanager");
+
+const dbname = "people";
+
+async function load() {
+    return await dbmanager.load(dbname);
+}
+
+async function save(data) {
+    return await dbmanager.save(dbname, data);
+}
 
 module.exports = {
-    load: async function () {
-        var data = await fs.readFile("./db/people.json", "utf8");
-        if (data.includes(ids.users.chocola)) {
-            try {
-                return JSON.parse(await fs.readFile("./db/people.json"));
-            }
-            catch (err) {
-                console.log(err);
-            }
-        }
-        else {
-            console.log("JSON error, attempting restore");
-            try {
-                var backup = JSON.parse(await fs.readFile("/home/badmin/backup/Mei/db/people.json"));
-                await fs.writeFile("/home/badmin/Bots/Mei/db/people.json", JSON.stringify(backup, null, "\t"));
-                console.log("Restore Successful");
-                return JSON.parse(await fs.readFile("/home/badmin/Bots/Mei/db/people.json"));
-            }
-            catch (err) {
-                console.log(err);
-            }
-        }
-    },
-    save: async function (data) {
-        await fs.writeFile("./db/people.json", JSON.stringify(data, null, "\t"));
-    }
+    load,
+    save
 };
