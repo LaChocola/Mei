2e53743a3c658899db7d71c38f982c87d06c47e1
Natalie Fearnley <nfearnley@gmail.com>
2020-02-02 00:18:17 -0500

    cleanup aesthetics

--- commands/aesthetics.js
+++ commands/aesthetics.js
@@ -5,19 +5,18 @@ const aesthetics = require("aesthetics");
 module.exports = {
     // eslint-disable-next-line no-unused-vars
     main: async function(Bot, m, args, prefix) {
-        var msg = m.cleanContent.replace(`${prefix}aesthetics `, "");
-        if (m.content === `${prefix}aesthetics`) {
-            msg = "You need to add something to say";
+        var msg = m.cleanContent.slice(`${prefix}aesthetics`.length).trim();
+        if (msg === "") {
+            await m.reply("You need to add something to say");
+            return;
         }
-        var text = aesthetics(msg);
-        var embed = "**" + text + "**";
-        Bot.createMessage(m.channel.id, {
+
+        await m.reply({
             embed: {
                 color: 0xA260F6,
-                description: embed
+                description: `**${aesthetics(msg)}**`
             }
         });
-        return;
     },
     help: "Vaporwave Text"
 };
