6760fe2635fa4b188b6a591dd1ffb615c1630d07
Natalie Fearnley <nfearnley@gmail.com>
2020-04-10 03:59:02 -0400

    updated ban permissions

--- commands/ban.js
+++ commands/ban.js
@@ -3,76 +3,41 @@
 const escapeStringRegexp = require("escape-string-regexp");
 
 const serversdb = require("../servers");
-const ids = require("../ids");
+const misc = require("../misc");
 
 module.exports = {
     // eslint-disable-next-line no-unused-vars
     main: async function(Bot, m, args, prefix) {
-        var data = await serversdb.load();
+        var guildsdata = await serversdb.load();
 
         var name1 = m.cleanContent.replace(new RegExp(escapeStringRegexp(prefix) + "names ", "i"), "");
         var argsArray = args.split(" ");
-        async function isMod(member, guild) {
-            if (data[guild.id]) {
-                if (data[guild.id].owner !== guild.ownerID) {
-                    Bot.createMessage(m.channel.id, "New server owner detected, updating database.").then((msg) => {
-                        return setTimeout(function() {
-                            Bot.deleteMessage(m.channel.id, msg.id, "Timeout");
-                        }, 5000);
-                    });
-                    data[guild.id].owner = guild.ownerID;
-                    await serversdb.save(data);
-                }
-                if (data[guild.id].mods) {
-                    if (data[guild.id].mods[member.id]) {
-                        return true;
-                    }
-                }
-                if (m.author.id === data[guild.id].owner || m.author.id === guild.ownerID) {
-                    return true;
-                }
-                if (data[guild.id].modRoles) {
-                    var memberRoles = member.roles;
-                    var mod = false;
-                    for (let role of memberRoles) {
-                        if (data[guild.id].modRoles[role]) {
-                            mod = true;
-                        }
-                    }
-                    if (mod) {
-                        return true;
-                    }
-                }
-            }
-            else {
-                var perms = await guild.members.get(member.id).permission.json;
-                var pArray = ["banMembers", "administrator", "manageGuild"];
-                if (perms[pArray[0]] || perms[pArray[1]] || perms[pArray[2]]) {
-                    return true;
-                }
-                return false;
-            }
-        }
-        var modCheck = await isMod(m.channel.guild.members.get(m.author.id), m.channel.guild);
-        var responses = ["Are you a real villan?", "Have you ever caught a good guy? \nLike a real super hero?", "Have you ever tried a disguise?", "What are you doing?!?!?!", "*NO!*, Don't touch that!", "Fuck Off", "Roses are red\nfuck me ;) "];
-        var response = responses[Math.floor(Math.random() * responses.length)];
-        if (modCheck !== true && m.author.id !== ids.users.chocola) {
-            Bot.createMessage(m.channel.id, response).then((msg) => {
-                return setTimeout(function() {
-                    Bot.deleteMessage(m.channel.id, msg.id, "Timeout");
-                    Bot.deleteMessage(m.channel.id, m.id, "Timeout");
-                }, 5000);
-            });
+
+        var memberIsMod = misc.isMod(m.member, m.guild, guildsdata[m.guild.id]);
+        var hasPerms = misc.hasSomePerms(m.member, ["administrator", "manageRoles"]);
+        if (!(memberIsMod || hasPerms)) {
+            var responses = [
+                "Are you a real villan?",
+                "Have you ever caught a good guy?\nLike a real super hero?",
+                "Have you ever tried a disguise?",
+                "What are you doing?!?!?!",
+                "*NO!*, Don't touch that!",
+                "Fuck Off",
+                "Roses are red\nfuck me ;) "
+            ];
+            var response = misc.choose(responses);
+            m.reply(response, 5000);
+            m.deleteIn(5000);
             return;
         }
-        var hands = [":ok_hand::skin-tone-1:", ":ok_hand::skin-tone-2:", ":ok_hand::skin-tone-3:", ":ok_hand::skin-tone-4:", ":ok_hand::skin-tone-5:", ":ok_hand:"];
-        var hand = hands[Math.floor(Math.random() * hands.length)];
+
         function isThisUsernameThatUsername(member) {
             var memberName = member.nick || member.username;
             if (memberName.toLowerCase() === name1.toLowerCase()) {
                 return true;
             }
         }
+
         var member = m.guild.members.find(isThisUsernameThatUsername);
         var mentioned = m.mentions[0] || member;
         var name;
@@ -109,7 +74,7 @@ module.exports = {
             if (undo) {
                 Bot.unbanGuildMember(m.channel.guild.id, id, "Unbanned by: " + guardian)
                     .then(() => {
-                        Bot.createMessage(m.channel.id, hand + " Successful Unbanned: " + name + " (" + id + ")").then((msg) => {
+                        Bot.createMessage(m.channel.id, misc.chooseHand() + " Successful Unbanned: " + name + " (" + id + ")").then((msg) => {
                             return setTimeout(function() {
                                 Bot.deleteMessage(m.channel.id, msg.id, "Timeout");
                                 Bot.deleteMessage(m.channel.id, m.id, "Timeout");
@@ -148,7 +113,7 @@ module.exports = {
             else if (id || name !== guardian) {
                 Bot.banGuildMember(m.channel.guild.id, id, 0, reason)
                     .then(() => {
-                        Bot.createMessage(m.channel.id, hand + " Successful banned: " + name + " (" + id + ")").then((msg) => {
+                        Bot.createMessage(m.channel.id, misc.chooseHand() + " Successful banned: " + name + " (" + id + ")").then((msg) => {
                             return setTimeout(function() {
                                 Bot.deleteMessage(m.channel.id, msg.id, "Timeout");
                             }, 5000);
