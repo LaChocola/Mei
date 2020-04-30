fbd5a6ceeec3fe33e3736658d7929d58998b7bb5
Chocola <thenekoluv@gmail.com>
2020-04-30 04:52:39 -0500

    Updated Mei licensing to GPL

--- Mei.js
+++ Mei.js
@@ -25,6 +25,18 @@ conf.load();
 var bot = Eris(conf.tokens.mei);
 var commandContentsMap = {};
 
+console.log(`Copyright (C) ${new Date().getFullYear()} Chocola
+    
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.\n`)
+
 if (!fs) {
     console.log("Mei requires Node.js version number 10 or above");
     return;
