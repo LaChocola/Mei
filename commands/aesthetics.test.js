afd492e84e866ddda8db9a2dc3501401d0738146
Natalie Fearnley <nfearnley@gmail.com>
2020-05-29 23:22:43 -0400

    Expanded aesthetics tests

--- commands/aesthetics.test.js
+++ commands/aesthetics.test.js
@@ -10,3 +10,16 @@ test("!aesthetics", async function() {
 
     expect(m.reply).toBeCalledWith("You need to add something to say");
 });
+
+test("!aesthetics This should be printed in FULL-WIDTH characters.", async function() {
+    var { bot, m, args, prefix } = setupCmd("aesthetics", "This should be printed in FULL-WIDTH characters.");
+
+    await aesthetics.main(bot, m, args, prefix);
+
+    expect(m.reply).toBeCalledWith({
+        embed: {
+            color: 0xA260F6,
+            description: "**Ｔｈｉｓ　ｓｈｏｕｌｄ　ｂｅ　ｐｒｉｎｔｅｄ　ｉｎ　ＦＵＬＬ－ＷＩＤＴＨ　ｃｈａｒａｃｔｅｒｓ．**"
+        }
+    });
+});
