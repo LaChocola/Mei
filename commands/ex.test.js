1ed6959d2f249d4b42037f2076d9de2e083157b7
Natalie Fearnley <nfearnley@gmail.com>
2020-05-31 01:56:07 -0400

    fixed !ex test

--- commands/ex.test.js
+++ commands/ex.test.js
@@ -1,10 +1,14 @@
 "use strict";
 
+const request = require("request-promise");
+
 const ex = require("./ex");
 
 // Stub test to be expanded on later
 test("!ex", async function() {
     var { bot, m, args, prefix } = setupCmd("ex");
+
+    request.mockResolvedValue("");
 
     await ex.main(bot, m, args, prefix);
 
