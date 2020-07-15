7782d422560cbea8f229da1d03f32509555ae466
Natalie Fearnley <nfearnley@gmail.com>
2020-07-15 16:08:39 -0400

    simplified !ping command so it won't break tests

--- commands/ping.js
+++ commands/ping.js
@@ -4,10 +4,9 @@ module.exports = {
     // eslint-disable-next-line no-unused-vars
     main: async function(bot, m, args, prefix) {
         var time = process.hrtime();
-        bot.createMessage(m.channel.id, "Pinging!").then(function(msg) {
-            var ms = Math.round(process.hrtime(time)[1] / 1000000);
-            bot.editMessage(msg.channel.id, msg.id, "Pong! `" + ms + "ms`");
-        });
+        var msg = await bot.createMessage(m.channel.id, "Pinging!");
+        var ms = Math.round(process.hrtime(time)[1] / 1000000);
+        await msg.edit("Pong! `" + ms + "ms`");
     },
     help: "Bot Delay"
 };
