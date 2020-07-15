893f5c3303d8a54971f7ca3c7a461c7970f82200
Natalie Fearnley <nfearnley@gmail.com>
2020-07-15 15:29:26 -0400

    fixed extra jest messages popping up for !play tests

--- commands/play.test.js
+++ commands/play.test.js
@@ -5,6 +5,22 @@ const play = require("./play");
 jest.mock("../misc");
 jest.mock("../servers");
 
+beforeEach(function() {
+    // Mock console to prevent messages from showing up during testing
+    jest.spyOn(console, "debug").mockImplementation();
+    jest.spyOn(console, "log").mockImplementation();
+    jest.spyOn(console, "error").mockImplementation();
+    jest.spyOn(console, "warn").mockImplementation();
+});
+
+afterEach(function() {
+    // Unmock console
+    console.debug.mockRestore();
+    console.log.mockRestore();
+    console.error.mockRestore();
+    console.warn.mockRestore();
+});
+
 // Stub test to be expanded on later
 test("!play", async function() {
     var { bot, m, args, prefix } = setupCmd("play");
